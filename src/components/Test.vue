c34ac73d41592b834ab8f86aa0cface667c0fd48
Daniel González <d.g.g69@me.com>
2017-02-10 00:24:20 -0600

    test vue

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='test'>
+    <div class="other-class"><span class="a-class"></span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+
+}
+</script>
+
+<style scoped>
+
+</style>
