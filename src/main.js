ac54bd277296d699e2fb77d3a7069b653c355184
Daniel González <d.g.g69@me.com>
2017-02-14 16:38:00 -0600

    Add GTM

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueGtm from 'vue-gtm';
 import App from './App';
 import router from './router';
 
@@ -10,6 +11,12 @@ import store from './store/someStore';
 
 window.api = api;
 // TODO: Update the use of Api and Store
+
+Vue.use(VueGtm, {
+  debug: true, // Whether or not display console logs debugs (optional)
+  vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+  //ignoredViews: ['homepage'], If router you can exclude some routes names (optional)
+});
 
 /* eslint-disable no-new */
 new Vue({
